04e62889258b8068463f0831f03daf426304b343
Jim Pryor <dubiousjim@gmail.com>
2012-05-04 01:08:50 -0400

    move LICENSE to separate file

--- runawk.c
+++ runawk.c
@@ -1,31 +1,11 @@
 /*
- * Copyright (c) 2007-2011 Aleksey Cheusov <vle@gmx.net>
- * Copyright (c) 2012 Jim Pryor <dubiousjim@gmail.com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- *
- * Lite versions starting at 0.30.0 by Jim Pryor
- * Adapted from Runawk 1.4.0 at http://runawk.sourceforge.net/
- */
-
+ * Runawk lite version 0.30.0 by Jim Pryor
+ *   https://github.com/dubiousjim/awkenough
+ * Adapted from Runawk 1.4.0 by Aleksey Cheusov
+ *   http://runawk.sourceforge.net/
+ * Released under the MIT license; see LICENSE
+ * Date: Fri May  4 01:04:30 EDT 2012
+ */ 
 
 #include <stdlib.h>
 #include <string.h>
